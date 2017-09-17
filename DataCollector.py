ea12bb473d4c6b8d13f4f230c5bda02d461492e6
Andre <andree_kaba93@hotmail.com>
2017-09-17 13:28:24 -0400

    try to print all top level comments

--- DataCollector.py
+++ DataCollector.py
@@ -1,33 +1,38 @@
-import praw
+import praw,sys
 
 def main():
+
+    #redirect to log file
+    sys.stdout = open('log.txt', 'w')
+
     # obtain a reddit instance
-    reddit = praw.Reddit(client_id='f2gYRGChaUFsrQ',
+    reddit = praw.Reddit(client_id='f2gYedRGChaUFsrQ',
                         client_secret='iJwwmN1GkxLHU_CYnMXqXtDlPWs',
                         user_agent='my user agent',
                         username='DialogueDataset',
                         password='comp551')
 
-    '''
-    with open("SpanishSubreddits.txt") as file:
-        for line in file:
-            parse(line)
-    file.close()
-    '''
-    DFS("catalunya")
+    DFS(reddit.subreddit("catalonia").top('all'))
 
 def DFS(subreddit):
-    for submission in reddit.subreddit(subreddit).top('all'):
-        visited = Set([])
-        stack = []
-        submission.comments.replace_more(limit=0)  
-        stack.append(submission.comments[1])
-        while not stack:
-            current = stack.pop()
-            visited.add(current)
-            add_to_XML(current)
-            for reply in comment.replies:
-                stack.append(reply)
+    for submission in subreddit:
+        submission.comments.replace_more(limit=0)
+        for top_level_comment in submission.comments:
+            print(top_level_comment.body)
+        '''
+        if len(submission.comments)>0:
+            visited = set([])
+            stack = []
+            stack.append(submission.comments.__getitem__(0))
+            while stack:
+                current = stack.pop()
+                print(current)
+                visited.add(current)
+                add_to_XML(current)
+                for reply in current.replies:
+                    stack.append(reply)
+        '''
+
 
 #placeholder
 def add_to_XML(comment):
