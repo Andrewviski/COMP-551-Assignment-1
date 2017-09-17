df9322d9568c345514b977c103c06cfe6373c9ba
Isaac Sultan <isaacsultan1@gmail.com>
2017-09-17 12:32:52 -0400

    Began implementation of DFS

--- DataCollector.py
+++ DataCollector.py
@@ -1,14 +1,37 @@
 import praw
 
-# obtain a reddit instance
-reddit = praw.Reddit(client_id='f2gYRGChaUFsrQ',
-                    client_secret='iJwwmN1GkxLHU_CYnMXqXtDlPWs',
-                    user_agent='my user agent',
-                    username='DialogueDataset',
-                    password='comp551')
+def main():
+    # obtain a reddit instance
+    reddit = praw.Reddit(client_id='f2gYRGChaUFsrQ',
+                        client_secret='iJwwmN1GkxLHU_CYnMXqXtDlPWs',
+                        user_agent='my user agent',
+                        username='DialogueDataset',
+                        password='comp551')
 
-# trying to play around with some random subreddit
-for submission in reddit.subreddit('learnpython').hot(limit=10):
-    print(submission.title)
-    print(submission.score)
-    print(submission.id)
+    '''
+    with open("SpanishSubreddits.txt") as file:
+        for line in file:
+            parse(line)
+    file.close()
+    '''
+    DFS("catalunya")
+
+def DFS(subreddit):
+    for submission in reddit.subreddit(subreddit).top('all'):
+        visited = Set([])
+        stack = []
+        submission.comments.replace_more(limit=0)  
+        stack.append(submission.comments[1])
+        while not stack:
+            current = stack.pop()
+            visited.add(current)
+            add_to_XML(current)
+            for reply in comment.replies:
+                stack.append(reply)
+
+#placeholder
+def add_to_XML(comment):
+    return None
+
+if __name__ == "__main__":
+    main()
