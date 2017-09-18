716edca7059c5e1d495fb11276b6fd2185f5ecb8
Andre <andree_kaba93@hotmail.com>
2017-09-18 17:04:32 -0400

    working version :)

--- DataCollector.py
+++ DataCollector.py
@@ -1,42 +1,61 @@
-import praw,sys
+import praw,sys,time
+
+ofile = open("log.xml", "w")
 
 def main():
+    ifile=open("SpanishSubreddits.txt","r")
 
-    #redirect to log file
-    sys.stdout = open('log.txt', 'w')
 
-    # obtain a reddit instance
-    reddit = praw.Reddit(client_id='f2gYedRGChaUFsrQ',
-                        client_secret='iJwwmN1GkxLHU_CYnMXqXtDlPWs',
+    reddit = praw.Reddit(client_id='OlfGGYW2I06cVQ',
+                        client_secret='FneP5eGqBL1x3pV0GifzbieYy_E',
                         user_agent='my user agent',
                         username='DialogueDataset',
                         password='comp551')
+    for line in ifile:
+        line=line.replace('\n',"")
 
-    DFS(reddit.subreddit("catalonia").top('all'))
+        subreddit=reddit.subreddit(line).top('all')
+        print("Extracting " + line + "(" + subreddit.url + ")" + " Subreddit...")
+        DFS(subreddit)
+        print("Done Extracting " + line +"!\n\n")
+        print('-----------------------------=============================================-----------------------------')
+
+
+def parse_replies(comment,users,uid):
+    for reply in comment.replies:
+        if (reply.author is not None) and (not reply.author.name in users):
+            users[reply.author.name]=uid
+            uid+=1
+
+        if reply.author is not None:
+            ofile.write("<utt uid=\"" + str(users[reply.author.name]) + "\">")
+            ofile.write(reply.body)
+            ofile.write("</utt>")
+
+            reply.replies.replace_more(limit=0)
+            for reply2 in reply.replies:
+                parse_replies(reply2,users,uid)
+
+
 
 def DFS(subreddit):
+
     for submission in subreddit:
+        print("["+submission.title+"]: ( url: "+submission.url+" ) {id: "+submission.id+" }")
+        ofile.write("<dialog>")
         submission.comments.replace_more(limit=0)
-        for top_level_comment in submission.comments:
-            print(top_level_comment.body)
-        '''
-        if len(submission.comments)>0:
-            visited = set([])
-            stack = []
-            stack.append(submission.comments.__getitem__(0))
-            while stack:
-                current = stack.pop()
-                print(current)
-                visited.add(current)
-                add_to_XML(current)
-                for reply in current.replies:
-                    stack.append(reply)
-        '''
+        for comment in submission.comments:
+            ofile.write("<s>")
+            users={}
+            if comment.author is not None:
+                users[comment.author.name]=0
+                ofile.write("<utt uid=\""+str(users[comment.author.name])+"\">")
+                ofile.write(comment.body)
+                ofile.write("</utt>")
+            parse_replies(comment,users,1)
+            ofile.write("</s>")
+        ofile.write("</dialog>")
 
-
-#placeholder
-def add_to_XML(comment):
-    return None
 
 if __name__ == "__main__":
     main()
